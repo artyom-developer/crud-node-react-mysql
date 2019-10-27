d27e6f67b8d7bcdd25666d2b47cb46998f38432f
artyom <felipeperdomo32@hotmail.com>
2019-10-26 20:19:49 -0500

    subida de node

--- nodebackend/src/controllers/EmployeeController.js
+++ nodebackend/src/controllers/EmployeeController.js
@@ -8,6 +8,59 @@ var Role = require('../model/Role');
 // para migrar por si no tiene tablas
 sequelize.sync()
 
+controllers.delete = async (req,res) => {
+
+  // parameter post
+  const { id } = req.body;
+  // delete sequelize
+  const del = await Employee.destroy({
+    where: { id: id }
+  })
+  res.json({success:true, deleted:del, message:"Deleted successful"});
+
+}
+
+controllers.update = async (req, res) => {
+  // parameter id get
+  const { id } = req.params;
+  // parameter post
+  const { name, email, phone, address, role } = req.body;
+  // update data
+  const data = await Employee.update({
+    name: name,
+    email:email,
+    address:address,
+    phone:phone,
+    roleId:role
+  },{
+    where: { id: id}
+  })
+  .then( function (data){
+    return data;
+  })
+  .catch(error => {
+    return error;
+  })
+
+  res.json({ success:true, data: data, message: "Updated successful"});
+
+}
+
+controllers.get = async (req, res) => {
+  const { id } = req.params;
+  const data = await Employee.findAll({
+    where: { id: id},
+    include: [ Role ]
+  })
+  .then( function(data){
+    return data;
+  })
+  .catch(error => {
+    return error;
+  })
+  res.json({success:true, data:data});
+}
+
 controllers.list = async (req,res) => {
   const data = await Employee.findAll({
     include: [ Role ]
@@ -49,6 +102,5 @@ controllers.create = async (req,res) => {
   })
 
 }
-
 
 module.exports = controllers;
--- nodebackend/src/routes/EmployeeRoute.js
+++ nodebackend/src/routes/EmployeeRoute.js
@@ -5,6 +5,9 @@ const EmployeeController = require('../controllers/EmployeeController');
 
 router.get('/list',EmployeeController.list);
 router.post('/create',EmployeeController.create);
+router.get('/get/:id', EmployeeController.get);
+router.post('/update/:id',EmployeeController.update);
+router.post('/delete',EmployeeController.delete);
 // router.get('/test',EmployeeController.test );
 // router.post('/create',(req,res)=>{
 //   res.json({
